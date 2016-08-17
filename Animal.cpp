a000cd409d11ce33f2ebf0fe786d4e3711920489
abbreviatedman <colin.jaffe@gmail.com>
2016-08-17 14:53:33 -0400

    MUCH more compartmentalized code.
    Moves the breeding function out of the Animals class and into main.cpp, passing the Animals by reference instead of using member functions. Breaks the breeding function into separate functions for different variables and for printing.

--- Animal.cpp
+++ Animal.cpp
@@ -22,89 +22,89 @@ Animal::~Animal()
 	puts ("I am become death, destructor of Animals.");
 }
 
-void Animal::breedAnimal(Animal dad, Animal mom)
-{
-
-	//combine species
-	srand(time(NULL));
-	if (rand() % 2)
-	{
-		_species = dad._species;
-	}
-	else
-	{
-		_species = mom._species;
-	}
-
-	//pick gender and assign appropriate words and name
-	if (rand() % 2)
-	{
-		_isFemale = false;
-		int namerand = rand() % 5;
-		switch (namerand)
-		{
-		default:
-		case 0:
-			_name = "Rover";
-			break;
-		case 1:
-			_name = "Oscar";
-			break;
-		case 2:
-			_name = "Mellow";
-			break;
-		case 3:
-			_name = "Maverick";
-			break;
-		case 4:
-			_name = "Buddy";
-			break;
-		}
-
-	}
-	else
-	{
-		_isFemale = true;
-		int namerand = rand() % 5;
-		switch (namerand)
-		{
-		default:
-		case 0:
-			_name = "Lassie";
-			break;
-		case 1:
-			_name = "Maisie";
-			break;
-		case 2:
-			_name = "Goldberry";
-			break;
-		case 3:
-			_name = "Lucy";
-			break;
-		case 4:
-			_name = "Bee";
-			break;
-		}
-	}
-
-
-	//combine sizes
-	_size = (dad._size + mom._size) / 2;
-
-	//pick sound
-	if (_species == "dog")
-		{
-			_sound = "woof";
-		}
-		else
-		{
-			_sound = "glurb";
-		}
-	printf("A %s was born to a %s named %s and a %s named %s! %s name is %s. Now fully grown, our new %s weighs %d pounds. %s says %s.\n",
-			_isFemale ? "daughter" : "son", mom._species.c_str(), mom._name.c_str(), dad._species.c_str(), dad._name.c_str(),
-			_isFemale ? "Her" : "His", _name.c_str(),
-			_species.c_str(), _size, _isFemale ? "She" : "He", _sound.c_str());
-}
+//void Animal::breedAnimal(Animal dad, Animal mom)
+//{
+//
+//	//combine species
+//	srand(time(NULL));
+//	if (rand() % 2)
+//	{
+//		_species = dad._species;
+//	}
+//	else
+//	{
+//		_species = mom._species;
+//	}
+//
+//	//pick gender and assign appropriate words and name
+//	if (rand() % 2)
+//	{
+//		_isFemale = false;
+//		int namerand = rand() % 5;
+//		switch (namerand)
+//		{
+//		default:
+//		case 0:
+//			_name = "Rover";
+//			break;
+//		case 1:
+//			_name = "Oscar";
+//			break;
+//		case 2:
+//			_name = "Mellow";
+//			break;
+//		case 3:
+//			_name = "Maverick";
+//			break;
+//		case 4:
+//			_name = "Buddy";
+//			break;
+//		}
+//
+//	}
+//	else
+//	{
+//		_isFemale = true;
+//		int namerand = rand() % 5;
+//		switch (namerand)
+//		{
+//		default:
+//		case 0:
+//			_name = "Lassie";
+//			break;
+//		case 1:
+//			_name = "Maisie";
+//			break;
+//		case 2:
+//			_name = "Goldberry";
+//			break;
+//		case 3:
+//			_name = "Lucy";
+//			break;
+//		case 4:
+//			_name = "Bee";
+//			break;
+//		}
+//	}
+//
+//
+//	//combine sizes
+//	_size = (dad._size + mom._size) / 2;
+//
+//	//pick sound
+//	if (_species == "dog")
+//		{
+//			_sound = "woof";
+//		}
+//		else
+//		{
+//			_sound = "glurb";
+//		}
+//	printf("A %s was born to a %s named %s and a %s named %s! %s name is %s. Now fully grown, our new %s weighs %d pounds. %s says %s.\n",
+//			_isFemale ? "daughter" : "son", mom._species.c_str(), mom._name.c_str(), dad._species.c_str(), dad._name.c_str(),
+//			_isFemale ? "Her" : "His", _name.c_str(),
+//			_species.c_str(), _size, _isFemale ? "She" : "He", _sound.c_str());
+//}
 
 
 
--- main.cpp
+++ main.cpp
@@ -11,12 +11,123 @@ ballo
 using namespace std;
 
 
+
+
+void breedAnimal(Animal & child, Animal dad, Animal mom);
+void setSpeciesAndSound(Animal & child, Animal dad, Animal mom);
+void setGenderAndName(Animal & child, Animal dad, Animal mom);
+void setSize(Animal & child, Animal dad, Animal mom);
+void printAnimal(Animal child, Animal dad, Animal mom);
+
+
+void breedAnimal(Animal & child, Animal dad, Animal mom)
+{
+	setSpeciesAndSound(child, dad, mom);
+	setGenderAndName(child, dad, mom);
+	setSize(child, dad, mom);
+	printAnimal(child, dad, mom);
+}
+
+
+void setSpeciesAndSound(Animal & child, Animal dad, Animal mom)
+{
+	//combine species
+	srand(time(NULL));
+	if (rand() % 2)
+	{
+		child._species = dad._species;
+	}
+	else
+	{
+		child._species = mom._species;
+	}
+
+	//pick sound
+	if (child._species == "dog")
+		{
+			child._sound = "woof";
+		}
+		else
+		{
+			child._sound = "glurb";
+		}
+}
+
+void setGenderAndName(Animal & child, Animal dad, Animal mom)
+{
+	//pick gender and assign appropriate words and name
+	if (rand() % 2)
+	{
+		child._isFemale = false;
+		int namerand = rand() % 5;
+		switch (namerand)
+		{
+		default:
+		case 0:
+			child._name = "Rover";
+			break;
+		case 1:
+			child._name = "Oscar";
+			break;
+		case 2:
+			child._name = "Mellow";
+			break;
+		case 3:
+			child._name = "Maverick";
+			break;
+		case 4:
+			child._name = "Buddy";
+			break;
+		}
+
+	}
+	else
+	{
+		child._isFemale = true;
+		int namerand = rand() % 5;
+		switch (namerand)
+		{
+		default:
+		case 0:
+			child._name = "Lassie";
+			break;
+		case 1:
+			child._name = "Maisie";
+			break;
+		case 2:
+			child._name = "Goldberry";
+			break;
+		case 3:
+			child._name = "Lucy";
+			break;
+		case 4:
+			child._name = "Bee";
+			break;
+		}
+	}
+}
+
+void setSize(Animal & child, Animal dad, Animal mom)
+{
+	//combine sizes
+	child._size = (dad._size + mom._size) / 2;
+}
+
+void printAnimal(Animal child, Animal dad, Animal mom)
+{
+	//print results of breeding
+	printf("A %s was born to a %s named %s and a %s named %s! %s name is %s. Now fully grown, our new %s weighs %d pounds. %s says %s.\n",
+			child._isFemale ? "daughter" : "son", mom._species.c_str(), mom._name.c_str(), dad._species.c_str(), dad._name.c_str(),
+			child._isFemale ? "Her" : "His", child._name.c_str(),
+			child._species.c_str(), child._size, child._isFemale ? "She" : "He", child._sound.c_str());
+}
+
 int main()
 {
 	Animal a ("woof", "dog", "Bob", 40, false);
 	Animal b ("woof", "dog", "Marcia", 70, true);
 	Animal c;
-	c.breedAnimal(a, b);
+	breedAnimal(c, a, b);
 	printf("Stopped breeding!\n");
 	return 0;
 }
