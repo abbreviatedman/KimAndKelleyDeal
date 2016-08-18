0498a7e1219f91dc7c68743475bad085133cbe93
abbreviatedman <colin.jaffe@gmail.com>
2016-08-18 16:38:47 -0400

    All animal orgy.
    Allows all animals to breed together, including new animals. Picks two animals at random and creates a new animal with characteristics from its parents.

--- Animal.cpp
+++ Animal.cpp
@@ -3,108 +3,292 @@
 #include <stdio.h>
 #include <string>
 #include <stdlib.h>
+#include <vector>
+#include <iostream>
 #include "Animal.h"
 using namespace std;
 
 Animal::Animal(): _sound("unknown"), _species("unknown"), _name("unknown"), _size(0), _isFemale(true)
 {
-	puts ("default constructor");
+//	puts ("default constructor");
 }
 
-Animal::Animal(string sound, string species, string name, int size, bool isFemale): _sound(sound), _species(species),
+Animal::Animal(string sound, string species, string name, int size, int isFemale): _sound(sound), _species(species),
 		_name(name), _size(size), _isFemale(isFemale)
 {
-	puts ("constructor with arguments");
+//	puts ("constructor with arguments");
 }
 
 Animal::~Animal()
 {
-	puts ("I am become death, destructor of Animals.");
+//	puts ("I am become death, destructor of Animals.");
 }
 
-//void Animal::breedAnimal(Animal dad, Animal mom)
-//{
-//
-//	//combine species
-//	srand(time(NULL));
-//	if (rand() % 2)
-//	{
-//		_species = dad._species;
-//	}
-//	else
-//	{
-//		_species = mom._species;
-//	}
-//
-//	//pick gender and assign appropriate words and name
-//	if (rand() % 2)
-//	{
-//		_isFemale = false;
-//		int namerand = rand() % 5;
-//		switch (namerand)
-//		{
-//		default:
-//		case 0:
-//			_name = "Rover";
-//			break;
-//		case 1:
-//			_name = "Oscar";
-//			break;
-//		case 2:
-//			_name = "Mellow";
-//			break;
-//		case 3:
-//			_name = "Maverick";
-//			break;
-//		case 4:
-//			_name = "Buddy";
-//			break;
-//		}
-//
-//	}
-//	else
-//	{
-//		_isFemale = true;
-//		int namerand = rand() % 5;
-//		switch (namerand)
-//		{
-//		default:
-//		case 0:
-//			_name = "Lassie";
-//			break;
-//		case 1:
-//			_name = "Maisie";
-//			break;
-//		case 2:
-//			_name = "Goldberry";
-//			break;
-//		case 3:
-//			_name = "Lucy";
-//			break;
-//		case 4:
-//			_name = "Bee";
-//			break;
-//		}
-//	}
-//
-//
-//	//combine sizes
-//	_size = (dad._size + mom._size) / 2;
-//
-//	//pick sound
-//	if (_species == "dog")
-//		{
-//			_sound = "woof";
-//		}
-//		else
-//		{
-//			_sound = "glurb";
-//		}
-//	printf("A %s was born to a %s named %s and a %s named %s! %s name is %s. Now fully grown, our new %s weighs %d pounds. %s says %s.\n",
-//			_isFemale ? "daughter" : "son", mom._species.c_str(), mom._name.c_str(), dad._species.c_str(), dad._name.c_str(),
-//			_isFemale ? "Her" : "His", _name.c_str(),
-//			_species.c_str(), _size, _isFemale ? "She" : "He", _sound.c_str());
-//}
+void Animal::randAnimal()
+{
+	switch (rand() % 5)
+	{
+	case 0:
+		_species = "dog";
+		_sound = "woof";
 
+		_isFemale = rand() % 2;
+		if (_isFemale)
+		{
+			_size = rand() % 120;
+			switch (rand() % 5)
+			{
+			default:
+			case 0:
+				_name = "Lassie";
+				break;
+			case 1:
+				_name = "Maisie";
+				break;
+			case 2:
+				_name = "Goldberry";
+				break;
+			case 3:
+				_name = "Lucy";
+				break;
+			case 4:
+				_name = "Bee";
+				break;
+			}
+		}
+		else
+		{
+			_size = rand() % 160;
+			switch (rand() % 5)
+				{
+				default:
+				case 0:
+					_name = "Rover";
+					break;
+				case 1:
+					_name = "Oscar";
+					break;
+				case 2:
+					_name = "Mellow";
+					break;
+				case 3:
+					_name = "Maverick";
+					break;
+				case 4:
+					_name = "Buddy";
+					break;
+				}
+		}
+		break;
+	case 1:
+		_species = "cat";
+		_sound = "meow";
+		_isFemale = rand() % 2;
+		if (_isFemale)
+		{
 
+			_size = rand() % 10 + 10;
+			switch (rand() % 5)
+			{
+			default:
+			case 0:
+				_name = "Tabby";
+				break;
+			case 1:
+				_name = "Lily";
+				break;
+			case 2:
+				_name = "Socks";
+				break;
+			case 3:
+				_name = "Snowball";
+				break;
+			case 4:
+				_name = "Janice";
+				break;
+			}
+		}
+		else
+		{
+
+			_size = rand() % 10 + 15;
+			switch (rand() % 5)
+				{
+				default:
+				case 0:
+					_name = "Mittens";
+					break;
+				case 1:
+					_name = "Vladimir";
+					break;
+				case 2:
+					_name = "Estragon";
+					break;
+				case 3:
+					_name = "Flash";
+					break;
+				case 4:
+					_name = "Tango";
+					break;
+				}
+		}
+		break;
+	case 2:
+		_species = "dinosaur";
+		_sound = "ROAR";
+		_isFemale = rand() % 2;
+		if (_isFemale)
+		{
+			_size = rand() % 2000 + 12000;
+			switch (rand() % 5)
+			{
+			default:
+			case 0:
+				_name = "Cera";
+				break;
+			case 1:
+				_name = "Chloe";
+				break;
+			case 2:
+				_name = "Petrova";
+				break;
+			case 3:
+				_name = "Pauline";
+				break;
+			case 4:
+				_name = "Posey";
+				break;
+			}
+		}
+		else
+		{
+			_size = rand() % 2000 + 16000;
+			switch (rand() % 5)
+				{
+				default:
+				case 0:
+					_name = "Littlefoot";
+					break;
+				case 1:
+					_name = "Ducky";
+					break;
+				case 2:
+					_name = "Spike";
+					break;
+				case 3:
+					_name = "Dino";
+					break;
+				case 4:
+					_name = "Barney";
+					break;
+				}
+		}
+		break;
+	case 3:
+		_species = "cow";
+		_sound = "moo";
+		_isFemale = rand() % 2;
+		if (_isFemale)
+		{
+			_size = rand() % 400 + 1200;
+			switch (rand() % 5)
+			{
+			default:
+			case 0:
+				_name = "Bessie";
+				break;
+			case 1:
+				_name = "Maisie";
+				break;
+			case 2:
+				_name = "Belle";
+				break;
+			case 3:
+				_name = "Daisy";
+				break;
+			case 4:
+				_name = "Isabella";
+				break;
+			}
+		}
+		else
+		{
+			_size = rand() % 400 + 2000;
+			switch (rand() % 5)
+				{
+				default:
+				case 0:
+					_name = "Ferdinand";
+					break;
+				case 1:
+					_name = "Duncan";
+					break;
+				case 2:
+					_name = "Doug";
+					break;
+				case 3:
+					_name = "Biff";
+					break;
+				case 4:
+					_name = "Milky White";
+					break;
+				}
+		}
+		break;
+	case 4:
+		_species = "ocelot";
+		_sound = "rawr";
+
+		_isFemale = rand() % 2;
+		if (_isFemale)
+		{
+			_size = rand() % 20 + 10;
+			switch (rand() % 5)
+			{
+			default:
+			case 0:
+				_name = "Omayra";
+				break;
+			case 1:
+				_name = "Olive";
+				break;
+			case 2:
+				_name = "Opal";
+				break;
+			case 3:
+				_name = "Olga";
+				break;
+			case 4:
+				_name = "Oneida";
+				break;
+			}
+		}
+		else
+		{
+		_size = rand() % 20 + 20;
+		switch (rand() % 5)
+				{
+				default:
+				case 0:
+					_name = "Omar";
+					break;
+				case 1:
+					_name = "Opie";
+					break;
+				case 2:
+					_name = "Olaf";
+					break;
+				case 3:
+					_name = "Orrin";
+					break;
+				case 4:
+					_name = "Chad";
+					break;
+				}
+		}
+		break;
+	}
+
+	printf("%s, %s, %s, %d, %s\n", _species.c_str(), _name.c_str(), _isFemale ? "female" : "male", _size, _sound.c_str());
+}
 
